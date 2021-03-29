93a05ecd35616b03ef129dd8caeb04cb03d7415e
dhyey6602 <dhyey6602patel@gmail.com>
2021-03-29 22:55:53 +0530

    added login/signup component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import React from "react";
-
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import LoginSignup from "./Pages/LoginSignup"
+import Home from "./Pages/Home"
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/LoginSignup" component={LoginSignup} />
+                {/* <Route path="/Profile" component={Dashboard} /> */}
+                {/* <Route path="/Recipe-page" component={Recipe} /> */}
+            </Switch>
+        </BrowserRouter>
   );
 }
 
