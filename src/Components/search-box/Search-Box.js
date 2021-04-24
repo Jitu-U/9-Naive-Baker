6616582ab511fa719d762e5e7376aeab4bf8f79e
Jeetesh Gamit <59947307+Jitu-U@users.noreply.github.com>
2021-04-24 11:30:12 +0530

    Public Profile Page added- Initiated Filters

--- src/Components/search-box/Search-Box.js
+++ src/Components/search-box/Search-Box.js
@@ -2,8 +2,7 @@ import React,{useState,useEffect} from 'react';
 import "./Search-Box.css";
 import { IoIosSearch } from "react-icons/io";
 import { IoFilterSharp } from "react-icons/io5"
-
-
+import { Switch } from '@material-ui/core';
 
 function SearchBox(){
 const [click, setClick] = useState(false);
@@ -19,8 +18,12 @@ const handleClick = () => setClick(!click);
                     "Search recipetitle, ingredients, cook Name"/>
 
                 </div>
-                <div className="search-button" >
-                    <IoFilterSharp className="search-icon" size={21}/>
+                <div className="Filters" >
+                    <IoFilterSharp className="filter-btn" size={21}/>
+                    <div className="Filter-Menu">
+                      <div className="Menu-item">  <Switch/> Veg Only</div>
+                      <div className="Menu-item"> <input className="F-input" type="text" placeholder="Enter ingredients"></input></div>
+                    </div>
                 </div>
 
         </div>
