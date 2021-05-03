7bd240281d14dab22546b6360abecb2f4b3a3452
Dhyey Gami <45729433+dhyey6602@users.noreply.github.com>
2021-05-03 21:15:28 +0530

    Login UI (#118)
    * updated dependencies
    
    * updated UI for login/signup
    
    * error msgs fixed

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -26,8 +26,16 @@ class Login extends React.Component {
         password : this.state.password
       })
       .then(res=>{
+        if(res.data.ok == "true")
+        {
           console.log(res.data)
+          alert("Successfully logged in")
           window.location = "http://localhost:3000/"
+        }
+        else
+        {
+          alert(res.data.err.msg);
+        }
       })
       .catch(res => { alert(res) })
     }
