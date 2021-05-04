15c53598b56b3918d9096329ee7e60476b57f991
Deep <46586516+DeepGajera91@users.noreply.github.com>
2021-05-04 16:15:18 +0530

    Home page Integration, update and navigate Recipe Detail page,  (#122)
    * bug fixing
    
    * dashboard integration
    
    * recipe search page
    
    * Home page integration, recipe detail page redirection

--- src/Pages/Recipe-page.js
+++ src/Pages/Recipe-page.js
@@ -2,37 +2,39 @@
 import React, { Component } from 'react'
 import RecipeDetail from "../Components/RecipeDetail/RecipeDetail"
 import "./Recipe-page.css"
-import {FcLikePlaceholder,FcLike} from 'react-icons/fc'
-import {MdPlaylistAdd,MdPlaylistAddCheck,MdShare} from 'react-icons/md'
+import { FcLikePlaceholder, FcLike } from 'react-icons/fc'
+import { MdPlaylistAdd, MdPlaylistAddCheck, MdShare } from 'react-icons/md'
 import Navbar from "../Components/Navbar/Navbar"
+import { Redirect, Link, useHistory, useLocation } from 'react-router-dom';
 
- class RecipePage extends Component {
-  render() {
-    return (
-        <>
-          <Navbar/>
-          <div className="container">
-          <RecipeDetail/>
-            <div className="Actions">
-              <div className="rAction">
-                <FcLikePlaceholder size={25} /> Like
+export default function RecipePage(props) {
+
+  const location = useLocation();
+  const Rec = location.state;
+
+  return (
+    <>
+      <Navbar />
+      <div className="container">
+        <RecipeDetail Rec={Rec}/>
+        <div className="Actions">
+          <div className="rAction">
+            <FcLikePlaceholder size={25} /> Like
                 </div>
-                  <div className="rAction">
-                      <MdPlaylistAdd size={25}/> save for later
+          <div className="rAction">
+            <MdPlaylistAdd size={25} /> save for later
                   </div>
-                  <div className="rAction">
-                    <MdShare/> Share
+          <div className="rAction">
+            <MdShare /> Share
                   </div>
-                  <div className="rAction">
-                    Share your Views
+          <div className="rAction">
+            Share your Views
                     <input type="text" placeholder="Share your views for this recipe" className="review-input"></input>
-                  </div>
+          </div>
 
-            </div>
-            </div>
-          </>
-    );
-  }
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default RecipePage;
