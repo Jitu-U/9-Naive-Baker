25f8f7e98468e0577a01efa254b68ab7dbd90901
DeepGajera91 <deepgajera91@gmail.com>
2021-05-04 15:57:46 +0530

    Home page integration, recipe detail page redirection

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,46 +1,58 @@
-
-
-// Home page  default and explore page 
-
+import React from 'react';
 import { BrowserRouter, Route, Switch, Redirect, Link, useHistory } from 'react-router-dom'
 import Navbar from "../Components/Navbar/Navbar"
 import Recipe from "../Components/RecipeCard/RecipeCard"
 import "./Home.css"
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import RecipeCard from '../Components/RecipeCard/RecipeCard';
 
 
-function Home() {
-  let history = useHistory();
+export default function Home() {
 
-  const recipe = () => {
-    history.push('/RecipePage')
-  }
+  const [loading, setLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const [recipes, setRecipes] = useState([]);
 
-    return (
-      <>
-      <div className="Home">
-      <div className="Home-header">
-        <Navbar className="navbar"/>
-      </div>
-      <div className="Home-Body-container">
-       <div className="Grid-Row">
-          <Recipe title="Nil" src="Nil"  onClick={recipe}/>
-          <Recipe/>
-          <Recipe/>
-        </div>
-        <div className="Grid-Row">
-          <Recipe title="Nil" src="Nil" />
-          <Recipe/>
-          <Recipe/>
-        </div>
-        <div className="Grid-Row">
-          <Recipe title="Nil" src="Nil" />
-          <Recipe/>
-          <Recipe/>
-        </div>
-      </div>
-      </div>
-      </>
-    );
-  }
+  useEffect(() => {
 
-  export default Home;
+    const fetch = () => {
+
+      setIsError(false);
+      setLoading(true);
+
+      axios
+        .get(`https://naivebakerr.herokuapp.com/recipe/all`)
+        .then(({ data }) => {
+          setRecipes(data.data.recipe);
+          setIsError(false);
+          setLoading(false);
+        }
+        )
+        .catch(err => {
+          setIsError(true);
+          console.log(err);
+        })
+    }
+
+    fetch();
+  }, [])
+
+  return (
+    <>
+      {isError && <div>Something went wrong ...</div>}
+      <Navbar />
+      {
+        loading === false &&
+          <div className="Home">
+            <div className="Home-Body-container">
+              {
+                recipes !== undefined && recipes.map(r => <RecipeCard r={r} />)
+              }
+            </div>
+          </div>
+      }
+    </>
+  );
+}
+
