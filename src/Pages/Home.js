e2e0f1febfbdcccbb950603099f12de7946e2cce
DeepGajera91 <deepgajera91@gmail.com>
2021-05-05 17:08:33 +0530

    speed up integration

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -8,49 +8,17 @@ import axios from 'axios';
 import RecipeCard from '../Components/RecipeCard/RecipeCard';
 
 
-export default function Home() {
-
-  const [loading, setLoading] = useState(true);
-  const [isError, setIsError] = useState(false);
-  const [recipes, setRecipes] = useState([]);
-
-  useEffect(() => {
-
-    const fetch = () => {
-
-      setIsError(false);
-      setLoading(true);
-
-      axios
-        .get(`https://naivebakerr.herokuapp.com/recipe/all`)
-        .then(({ data }) => {
-          setRecipes(data.data.recipe);
-          setIsError(false);
-          setLoading(false);
-        }
-        )
-        .catch(err => {
-          setIsError(true);
-          console.log(err);
-        })
-    }
-
-    fetch();
-  }, [])
-
+export default function Home({ recipes }) {
   return (
     <>
-      {isError && <div>Something went wrong ...</div>}
       <Navbar />
       {
-        loading === false &&
-          <div className="Home">
-            <div className="Home-Body-container">
-              {
-                recipes !== undefined && recipes.map(r => <RecipeCard r={r} />)
-              }
-            </div>
+        recipes.length !== 0 &&
+        <div className="Home">
+          <div className="Home-Body-container">
+            {recipes.map(r => <RecipeCard r={r} />)}
           </div>
+        </div>
       }
     </>
   );
--- src/Pages/UploadRecipe.js
+++ src/Pages/UploadRecipe.js
@@ -1,17 +1,13 @@
-//Upload page for Recipes
-
 import React from 'react';
 import Upload from "../Components/Upload_Recipe/upload"
 import Navbar from "../Components/Navbar/Navbar"
 
-function UploadRecipe() {
-      return(
-        <>
-        <Navbar/>
-        <Upload/>
-        </>
-      )
+export default function UploadRecipe({setRecipes}) {
+  return (
+    <>
+      <Navbar />
+      <Upload />
+    </>
+  )
 }
 
-
-export default UploadRecipe;
