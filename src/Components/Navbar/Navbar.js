f1b0fcf3e7a40dce8c5ae7f26063173b5c7b1ce1
Deep <46586516+DeepGajera91@users.noreply.github.com>
2021-05-08 17:18:59 +0530

    update:logout (#155)

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -2,21 +2,31 @@ import React, { useContext } from 'react';
 import { useHistory, Link, useLocation } from "react-router-dom"
 import "./Navbar.css"
 import { RiFileSearchLine } from 'react-icons/ri'
-import { AuthContext, UserContext } from '../../Contexts/context';
+import { AuthContext, UserContext, AuthDispatchContext, UserDispatchContext } from '../../Contexts/context';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
-import {IoLogOutOutline,IoInformationCircleOutline,IoLockClosedOutline} from "react-icons/io5"
-import {CgProfile} from "react-icons/cg"
+import { IoLogOutOutline, IoInformationCircleOutline, IoLockClosedOutline } from "react-icons/io5"
+import { CgProfile } from "react-icons/cg"
 
 
 function Navbar() {
 
   const { isAuth } = useContext(AuthContext);
   const { user } = useContext(UserContext);
+  const { setU } = useContext(UserDispatchContext);
+  const { setA } = useContext(AuthDispatchContext);
 
   const history = useHistory();
-  let signUp = () =>{
+
+  const signUp = () => {
     history.push("/LoginSignup")
+  }
+
+  const handlelogout = () => {
+    console.log("logout");
+    localStorage.removeItem('auth-token');
+    setA(false);
+    setU({});
   }
 
   const JoinNow = () => {
@@ -29,38 +39,38 @@ function Navbar() {
   const Profile = () => {
     const [anchorEl, setAnchorEl] = React.useState(null);
 
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
+    const handleClick = (event) => {
+      setAnchorEl(event.currentTarget);
+    };
 
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
+    const handleClose = () => {
+      setAnchorEl(null);
+    };
 
     return (
       <>
-      <div className="Profile-Button"  onClick={handleClick}>
+        <div className="Profile-Button" onClick={handleClick}>
 
-        <div className="Name">
-          {user.user.name}
+          <div className="Name">
+            {user.user.name}
+          </div>
+          <img className="Profile-Pic" src="https://lh3.googleusercontent.com/ogw/ADGmqu_zu--WffN4JlWGzZ0pulY4v67ZMm7FTfhJIYJhiTA=s64-c-mo"></img>
         </div>
-        <img className="Profile-Pic" src="https://lh3.googleusercontent.com/ogw/ADGmqu_zu--WffN4JlWGzZ0pulY4v67ZMm7FTfhJIYJhiTA=s64-c-mo"></img>
-      </div>
 
-      <Menu
-        style={{"border-radius":"13"}}
-        id="simple-menu"
-        anchorEl={anchorEl}
-        keepMounted
-        open={Boolean(anchorEl)}
-        onClose={handleClose}
-      >
-        <MenuItem onClick={()=> { history.push("/Dashboard")}} className="Menu-item"><CgProfile size={21}/>Dashboard</MenuItem>
-        <MenuItem onClick={()=> { history.push("/PrivacyPolicy")}} className="Menu-item"><IoLockClosedOutline size={21}/>Privacy Policy</MenuItem>
-        <MenuItem onClick={()=> { history.push("/About")}} className="Menu-item"><IoInformationCircleOutline size={21}/>About us</MenuItem>
-        <MenuItem onClick={()=> { history.push("/Dashboard")}} className="Menu-item" style={{"background-color":"red" ,"color":"white"}}>Logout <IoLogOutOutline size={20}/></MenuItem>
-      </Menu>
-        </>
+        <Menu
+          style={{ "border-radius": "13" }}
+          id="simple-menu"
+          anchorEl={anchorEl}
+          keepMounted
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+        >
+          <MenuItem onClick={() => { history.push("/Dashboard") }} className="Menu-item"><CgProfile size={21} />Dashboard</MenuItem>
+          <MenuItem onClick={() => { history.push("/PrivacyPolicy") }} className="Menu-item"><IoLockClosedOutline size={21} />Privacy Policy</MenuItem>
+          <MenuItem onClick={() => { history.push("/About") }} className="Menu-item"><IoInformationCircleOutline size={21} />About us</MenuItem>
+          <MenuItem onClick={handlelogout} className="Menu-item" style={{ "background-color": "red", "color": "white" }}>Logout <IoLogOutOutline size={20} /></MenuItem>
+        </Menu>
+      </>
 
     );
   }
@@ -76,11 +86,11 @@ function Navbar() {
             </div>
             {isAuth &&
               <Link className="upload-btn" to='/Upload'>
-                   &#8593;Upload
+                &#8593;Upload
               </Link>
             }
             {isAuth === true ?
-              <Profile/> :
+              <Profile /> :
               <JoinNow />
             }
           </div>
